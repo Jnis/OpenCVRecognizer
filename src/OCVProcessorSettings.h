228ad59621d52fd5ccb6c9a7882381c7bed5a086
Yanis Plumit <>
2023-07-13 16:47:07 +0300

    changed default scales

--- src/OCVProcessorSettings.h
+++ src/OCVProcessorSettings.h
@@ -10,7 +10,7 @@
 
 struct OCVProcessorSettings {
     float minVideoSize = 320; // rescale photo to this size. Ex. if 320, 800*600 -> 427*320 or 600*800 -> 320*427
-    std::vector<float> scales = {0.4, 0.5}; // image size to detect (minVideoSize * sale)
+    std::vector<float> scales = {0.4, 0.3}; // image size to detect (minVideoSize * sale) TODO: is it possible to use 1 image?
     
     double thresholdFire = 0.7; // % of similarity to start recognition
     double thresholdMin = 0.3; // minimum of % of similarity to additional checking
